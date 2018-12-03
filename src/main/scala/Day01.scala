93c7f1f569c47886d349cf65715b589794807bbc
Arnold Farkas <farkasarnold1981@gmail.com>
2018-12-03 16:50:04 +0100

    Day01 - optimized

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -2,17 +2,11 @@ object Day01 {
 
   def task1(ints: List[Int]): Int = ints.sum
 
-  def task2(ints: List[Int]): Int = {
-    val freqsInLoop = ints.indices.map(idx => ints.take(idx).sum)
-
-    val loopSum = ints.sum
-    val nums = Stream.from(0)
-    val freqs = nums.flatMap(idx => freqsInLoop.map(_ + loopSum * idx))
-
-    val duplicates = freqs.zipWithIndex.flatMap{case (freq, idx) =>
-        Some(freq).filter(freqs.take(idx).contains)
-    }
-
-    duplicates.head
-  }
+  def task2(ints: List[Int]): Int = Stream.iterate((0, 0, List.empty[Int])){
+    case (idx, freq, visitedFreqs) =>
+      (idx + 1, freq + ints(idx % ints.size), freq :: visitedFreqs)
+  }.flatMap{
+    case (idx, freq, visitedFreqs) =>
+      if (visitedFreqs contains freq) Some(freq) else None
+  }.head
 }
