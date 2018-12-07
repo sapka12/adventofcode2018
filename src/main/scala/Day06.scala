801fd328769983ef26b80f0130d7ed75b05b844a
Arnold Farkas <farkasarnold1981@gmail.com>
2018-12-07 23:08:57 +0100

    Day06 - task2

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -1,22 +1,17 @@
 object Day06 {
 
-  def manhattanDistance(a: (Int, Int), b: (Int, Int)): Int = (a._1 - b._1).abs + (a._2 - b._2).abs
 
+  private def manhattanDistance(a: (Int, Int), b: (Int, Int)): Int = (a._1 - b._1).abs + (a._2 - b._2).abs
 
-  def task1(lines: List[String]): Int = {
-
-    val coords = lines
+  private def getCoords(lines: List[String]) = {
+    lines
       .map(_.split(",").map(_.trim.toInt))
       .map(c => (c(0), c(1)))
+  }
 
-    def closest(x: Int, y: Int): Option[(Int, Int)] = {
-      val minDistance = coords.map(manhattanDistance(_, (x, y))).min
-      val minimals = coords.filter(manhattanDistance(_, (x, y)) == minDistance)
-      if (minimals.size == 1) Some(minimals.head) else None
-    }
-
-    val (minX, minY, maxX, maxY) = coords
-      .foldLeft((Int.MaxValue, Int.MaxValue, Int.MinValue, Int.MinValue)){case (aggr, coord) =>
+  private def edgeCoords(coords: List[(Int, Int)]) = {
+    coords
+      .foldLeft((Int.MaxValue, Int.MaxValue, Int.MinValue, Int.MinValue)) { case (aggr, coord) =>
         val (x, y) = coord
         val (_minX, _minY, _maxX, _maxY) = aggr
         (
@@ -26,6 +21,18 @@ object Day06 {
           _maxY max x
         )
       }
+  }
+
+  def task1(lines: List[String]): Int = {
+    val coords = getCoords(lines)
+
+    def closest(x: Int, y: Int): Option[(Int, Int)] = {
+      val minDistance = coords.map(manhattanDistance(_, (x, y))).min
+      val minimals = coords.filter(manhattanDistance(_, (x, y)) == minDistance)
+      if (minimals.size == 1) Some(minimals.head) else None
+    }
+
+    val (minX, minY, maxX, maxY) = edgeCoords(coords)
 
     val closestPoints = for {
       x <- minX to maxX
@@ -52,4 +59,18 @@ object Day06 {
 
     result
   }
+
+  def task2(lines: List[String], maxSumDistance: Int): Int = {
+    val coords = getCoords(lines)
+    def sumDistance(x: Int, y: Int): Int = coords.map(manhattanDistance((x, y), _)).sum
+    val (minX, minY, maxX, maxY) = edgeCoords(coords)
+
+    val distances = for {
+      x <- minX to maxX
+      y <- minY to maxY
+    } yield sumDistance(x, y)
+
+    distances.count(maxSumDistance > _)
+  }
+
 }
--- src/test/scala/Day06Spec.scala
+++ src/test/scala/Day06Spec.scala
@@ -5,20 +5,30 @@ import scala.io.Source
 class Day06Spec extends FlatSpec with Matchers {
 
   private val lines = Source.fromFile("day06-0-input.txt").getLines().toList
+  private val testInput = """1, 1
+                    |1, 6
+                    |8, 3
+                    |3, 4
+                    |5, 5
+                    |8, 9"""
+    .stripMargin.split("\n").toList
 
   behavior of "Day06"
 
   it should "test of task1" in {
-    Day06.task1("""1, 1
-                  |1, 6
-                  |8, 3
-                  |3, 4
-                  |5, 5
-                  |8, 9""".stripMargin.split("\n").toList) shouldBe 17
+    Day06.task1(testInput) shouldBe 17
   }
 
   it should "task1" in {
     Day06.task1(lines) shouldBe 6047
+  }
+
+  it should "test of task2" in {
+    Day06.task2(testInput, 32) shouldBe 16
+  }
+
+  it should "task2" in {
+    Day06.task2(lines, 10000) shouldBe 46320
   }
 
 }
